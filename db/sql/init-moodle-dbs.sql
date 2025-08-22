ec35af51540b0ebe5ebd441d66f7ebd39c47b9c7
AhnafMuttaki <ahnafx88@gmail.com>
2025-08-22 12:27:11 +0600

    dockerized done for 2 moodle version

---
+++ db/sql/init-moodle-dbs.sql
@@ -0,0 +1,3 @@
+-- SQL script to create databases for both Moodle versions
+CREATE DATABASE IF NOT EXISTS moodle405 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS moodle500 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
